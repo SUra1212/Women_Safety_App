06a1c9645d67c8a272ecb15dcef24793d857489c
Umayangi Ekanayake <umayangi1999@gmail.com>
2024-02-25 09:42:54 +0530

    Update child_home_page.dart

--- flutter_application_1/lib/child/bottom_screens/child_home_page.dart
+++ flutter_application_1/lib/child/bottom_screens/child_home_page.dart
@@ -417,7 +417,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Text(
-                          "In case of emergency, dial me",
+                          "Emergency services",
                           style: TextStyle(
                             fontSize: 22,
                             fontWeight: FontWeight.bold,
