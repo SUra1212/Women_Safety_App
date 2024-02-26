812a3a3cd8ca8cfb38a0115e8fc00561460054ba
Umayangi Ekanayake <umayangi1999@gmail.com>
2024-02-26 19:57:37 +0530

    added tutorial links

--- flutter_application_1/lib/child/bottom_screens/self_defence.dart
+++ flutter_application_1/lib/child/bottom_screens/self_defence.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/child/bottom_page.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SelfDefence extends StatefulWidget {
   @override
@@ -50,28 +51,53 @@ class _SelfDefenceState extends State<SelfDefence> {
                         Container(width: 20),
                         Expanded(
                           child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Container(height: 5),
-                              Text(
-                                "When to use it:",
-                                style: TextStyle(
-                                    fontSize: 20, color: Colors.black),
-                              ),
-                              Container(height: 5),
-                              Text(
-                                "Use this from a distance as a way of setting a strong body-language boundary (like if somebody's following you) or when you're engaged in sending physical strikes, Arthur says.",
-                              ),
-                              Text(
-                                "How to do it:",
-                                style: TextStyle(
-                                    fontSize: 20, color: Colors.black),
-                              ),
-                              Text(
-                                  "Stand with your feet shoulder-width apart and your hands by your sides. Keeping your toes pointed forward, take a natural step forward with your non-dominant leg, so your feet are staggered. Bend both knees slightly, elevate the back heel, bring your hands up with your hands about 12 inches from your face and your palms facing forward, tuck your chin, and shrug your shoulders slightly. Distribute your body weight between both feet, placing it more in the balls versus heels."),
-                              Container(height: 10),
-                            ],
-                          ),
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Container(height: 5),
+                                Text(
+                                  "When to use it:",
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.black),
+                                ),
+                                Container(height: 5),
+                                Text(
+                                  "Use this from a distance as a way of setting a strong body-language boundary (like if somebody's following you) or when you're engaged in sending physical strikes, Arthur says.",
+                                ),
+                                Text(
+                                  "How to do it:",
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.black),
+                                ),
+                                Text(
+                                  "Stand with your feet shoulder-width apart and your hands by your sides. Keeping your toes pointed forward, take a natural step forward with your non-dominant leg, so your feet are staggered. Bend both knees slightly, elevate the back heel, bring your hands up with your hands about 12 inches from your face and your palms facing forward, tuck your chin, and shrug your shoulders slightly. Distribute your body weight between both feet, placing it more in the balls versus heels.",
+                                ),
+                                Container(height: 10),
+                                GestureDetector(
+                                  onTap: () {
+                                    launch(
+                                        "https://youtu.be/Gx3_x6RH1J4?si=tz-x6N193Nx6Kcb9");
+                                  },
+                                  child: Row(
+                                    children: [
+                                      Image.asset(
+                                        'assets/yt.png', // Change to your YouTube logo image asset
+                                        height: 24, // Adjust size as needed
+                                        width: 24,
+                                      ),
+                                      SizedBox(
+                                          width:
+                                              5), // Add some space between the logo and the text
+                                      Text(
+                                        "Watch Tutorial",
+                                        style: TextStyle(
+                                          color: Colors.blue,
+                                          decoration: TextDecoration.underline,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ]),
                         ),
                       ],
                     ),
@@ -118,6 +144,31 @@ class _SelfDefenceState extends State<SelfDefence> {
                               Text(
                                   "Start in Ready Stance and keep your hands up. Rotating your left hip and shoulder, explosively extend your left palm straight out, with your fingertips straight up and elbow down. Keep your right hand up to protect your face. Immediately recoil your left arm, returning your shoulder and hip to the square ready stance.With your feet in the same position, send a palm strike with your right hand (be sure to rotate right hip), then try a left-right combination. If you are left-handed, practice a right-left combination. Your hand should stay open (i.e., don't make a fist) and the heel of your palm should make contact with the attacker's nose."),
                               Container(height: 10),
+                              GestureDetector(
+                                onTap: () {
+                                  launch(
+                                      "https://youtu.be/k9Jn0eP-ZVg?si=zC0VwDfENrex1a5Q");
+                                },
+                                child: Row(
+                                  children: [
+                                    Image.asset(
+                                      'assets/yt.png', // Change to your YouTube logo image asset
+                                      height: 24, // Adjust size as needed
+                                      width: 24,
+                                    ),
+                                    SizedBox(
+                                        width:
+                                            5), // Add some space between the logo and the text
+                                    Text(
+                                      "Watch Tutorial",
+                                      style: TextStyle(
+                                        color: Colors.blue,
+                                        decoration: TextDecoration.underline,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
                             ],
                           ),
                         ),
@@ -134,7 +185,7 @@ class _SelfDefenceState extends State<SelfDefence> {
                 ],
               ),
             ),
-             Card(
+            Card(
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(4),
               ),
@@ -166,17 +217,40 @@ class _SelfDefenceState extends State<SelfDefence> {
                               ),
                               Container(height: 5),
                               Text(
-                                "This is another last-resort move to create escape opportunities. It's particularly beneficial against somebody who's tall, Arthur says, especially if you can't reach their face for Palm-Heel Strikes."
-                              ),
+                                  "This is another last-resort move to create escape opportunities. It's particularly beneficial against somebody who's tall, Arthur says, especially if you can't reach their face for Palm-Heel Strikes."),
                               Text(
                                 "How to do it:",
                                 style: TextStyle(
                                     fontSize: 20, color: Colors.black),
                               ),
                               Text(
-                                "Start in Ready Stance, keeping hands up. Bend your right leg and drive your right knee straight up. As soon as the right knee is above your waistline, extend your hips (almost bend backward to generate power in the left leg/your loading leg) and kick your right shin directly to the attacker's groin, making sure to keep your toes pointed downward and out of the way. Immediately release your right foot behind you and return to Ready Stance."
-                                  ),
+                                  "Start in Ready Stance, keeping hands up. Bend your right leg and drive your right knee straight up. As soon as the right knee is above your waistline, extend your hips (almost bend backward to generate power in the left leg/your loading leg) and kick your right shin directly to the attacker's groin, making sure to keep your toes pointed downward and out of the way. Immediately release your right foot behind you and return to Ready Stance."),
                               Container(height: 10),
+                              GestureDetector(
+                                onTap: () {
+                                  launch(
+                                      "https://youtu.be/9XaoUlRDLBg?si=5FYBOt0C8nnCY8kx");
+                                },
+                                child: Row(
+                                  children: [
+                                    Image.asset(
+                                      'assets/yt.png', // Change to your YouTube logo image asset
+                                      height: 24, // Adjust size as needed
+                                      width: 24,
+                                    ),
+                                    SizedBox(
+                                        width:
+                                            5), // Add some space between the logo and the text
+                                    Text(
+                                      "Watch Tutorial",
+                                      style: TextStyle(
+                                        color: Colors.blue,
+                                        decoration: TextDecoration.underline,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
                             ],
                           ),
                         ),
@@ -186,7 +260,7 @@ class _SelfDefenceState extends State<SelfDefence> {
                 ],
               ),
             ),
-              Card(
+            Card(
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(4),
               ),
@@ -213,8 +287,7 @@ class _SelfDefenceState extends State<SelfDefence> {
                               ),
                               Container(height: 5),
                               Text(
-                                "Rely on the Hammerfist Punch move in almost any situation where you find yourself in danger, Jory says. It's most effective, though, when used to hit the attacker directly in the face, particularly the nose, jaw, or temple."
-                              ),
+                                  "Rely on the Hammerfist Punch move in almost any situation where you find yourself in danger, Jory says. It's most effective, though, when used to hit the attacker directly in the face, particularly the nose, jaw, or temple."),
                               Text(
                                 "How to do it:",
                                 style: TextStyle(
@@ -223,9 +296,33 @@ class _SelfDefenceState extends State<SelfDefence> {
                                 ),
                               ),
                               Text(
-                                "Start in Ready Stance. Raise your dominant hand up, bending at the elbow (like you're preparing to throw a ball). Rotate your hips toward attacker and bring your dominant arm down, smacking the attacker in face (aim for the nose) with the meaty bottom part of fist. If you're practicing this move, recoil to Ready Stance and repeat. In a real-world scenario, strike the punch and run while the attacker is incapacitated."
-                                  ),
+                                  "Start in Ready Stance. Raise your dominant hand up, bending at the elbow (like you're preparing to throw a ball). Rotate your hips toward attacker and bring your dominant arm down, smacking the attacker in face (aim for the nose) with the meaty bottom part of fist. If you're practicing this move, recoil to Ready Stance and repeat. In a real-world scenario, strike the punch and run while the attacker is incapacitated."),
                               Container(height: 10),
+                               GestureDetector(
+                                onTap: () {
+                                  launch(
+                                      "https://youtu.be/ORAOkP1h3R0?si=0LdYk38l9ifjbZfj");
+                                },
+                                child: Row(
+                                  children: [
+                                    Image.asset(
+                                      'assets/yt.png', // Change to your YouTube logo image asset
+                                      height: 24, // Adjust size as needed
+                                      width: 24,
+                                    ),
+                                    SizedBox(
+                                        width:
+                                            5), // Add some space between the logo and the text
+                                    Text(
+                                      "Watch Tutorial",
+                                      style: TextStyle(
+                                        color: Colors.blue,
+                                        decoration: TextDecoration.underline,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
                             ],
                           ),
                         ),
@@ -236,6 +333,7 @@ class _SelfDefenceState extends State<SelfDefence> {
                           width: 150,
                           fit: BoxFit.cover,
                         ),
+                        
                       ],
                     ),
                   ),
